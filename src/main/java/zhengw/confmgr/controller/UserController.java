fb8997e06044a59e8613e98fb9e949dd8fe59519
Zheng Wang <zhengw1989@hotmail.com>
2017-05-22 11:18:11 +0800

    修改分页ViewModel

--- src/main/java/zhengw/confmgr/controller/UserController.java
+++ src/main/java/zhengw/confmgr/controller/UserController.java
@@ -24,7 +24,6 @@ public class UserController extends BaseController {
 			page = super.DEFAULT_PAGE_INDEX;
 
 		Page<User> users = userService.findUserByPage(page, super.DEFAULT_PAGE_SIZE);
-//		model.addAttribute("users", users);
 
 		model.addAttribute("users", new PaginationViewModel<User>(users));
 
